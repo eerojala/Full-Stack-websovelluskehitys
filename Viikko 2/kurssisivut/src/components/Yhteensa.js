0355237ae637594cc4c5e4f54250e4494676df7e
eerojala <eero.ojala@helsinki.fi>
2018-01-29 16:26:02 +0200

    Tehtävä 2.1 tehty

---
+++ Viikko 2/kurssisivut/src/components/Yhteensa.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Yhteensa = ({ kurssi }) => {
+    return (
+        <p>yhteensä {kurssi.osat[0].tehtavia + kurssi.osat[1].tehtavia + kurssi.osat[2].tehtavia} tehtävää</p>
+    )
+} 
+
+export default Yhteensa
