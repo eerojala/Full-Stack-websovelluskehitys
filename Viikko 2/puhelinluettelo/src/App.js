63faa0c514ec30c8acde9d8f26ca8570ada48394
eerojala <eero.ojala@helsinki.fi>
2018-01-30 15:32:17 +0200

    tehtävä 2.11 tehty

--- Viikko 2/puhelinluettelo/src/App.js
+++ Viikko 2/puhelinluettelo/src/App.js
@@ -2,22 +2,26 @@ import React from 'react';
 import Entries from './components/Entries'
 import EntryForm from './components/EntryForm'
 import Filter from './components/Filter'
+import axios from 'axios'
 
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      persons: [
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Martti Tienari', number: '040-123456' },
-        { name: 'Arto Järvinen', number: '040-123456' },
-        { name: 'Lea Kutvonen', number: '040-123456' }
-      ],
+      persons: [],
       newName: '',
       newNumber: '',
       filter: ''
     }
+  }
+
+  componentWillMount() {
+    axios
+      .get("http://localhost:3001/persons")
+      .then(response => {
+        this.setState({ persons: response.data })
+      })
   }
 
   addPerson = (event) => {
